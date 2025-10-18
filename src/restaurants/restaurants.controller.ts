d121c282292b9ea239c31bf6857bbe6432b84028
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-18 20:39:52 +0300

    Created restaurant Controller

--- src/restaurants/restaurants.controller.ts
+++ src/restaurants/restaurants.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Query,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { RestaurantsService } from './restaurants.service';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { PaginationFilterDto } from '../dtos/pagination-filter.dto';
 
 @Controller('restaurants')
-export class RestaurantsController {}
+export class RestaurantsController {
+  constructor(private readonly restaurantsService: RestaurantsService) {}
+
+  @Post()
+  async create(@Body() body: CreateRestaurantDto) {
+    const createdRestaurant = await this.restaurantsService.create(body);
+    return {
+      message: 'Restaurant created successfully',
+      data: createdRestaurant,
+    };
+  }
+
+  @Get()
+  async findAll(
+    @Query() { page, limit }: PaginationFilterDto,
+    @Query('cuisine') cuisine?: string,
+  ) {
+    const { restaurantsCount, restaurants } =
+      await this.restaurantsService.findAll(page, limit, cuisine);
+    return {
+      pagination: {
+        page,
+        limit,
+        totalDocs: restaurantsCount,
+        totalPages: Math.ceil(restaurantsCount / limit),
+      },
+      data: restaurants,
+    };
+  }
+
+  @Get('nearby')
+  async findNearby(@Query('lat') lat: number, @Query('lng') lng: number) {
+    return await this.restaurantsService.findNearby(lat, lng);
+  }
+
+  @Get('/:identifier')
+  async findByIdOrSlug(@Param('identifier') identifier: string) {
+    return this.restaurantsService.findByIdOrSlug(identifier);
+  }
+
+  @Patch('/:id')
+  async update(@Param('id') id: string, @Body() body: any) {
+    return this.restaurantsService.update(id, body);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id: string) {
+    return this.restaurantsService.delete(id);
+  }
+}
