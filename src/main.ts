2c696c13e58f3d94653bd2379e4f85b3a817e4ad
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-18 18:59:17 +0300

    Updated the appModule to have expressApp

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const expressApp = express();
+
+  // Use raw Express middleware if needed
+  expressApp.use(express.json());
+  expressApp.get('/health', (req, res) => res.send('Server healthy!'));
+
+  // Run Nest on top of Express
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+  await app.listen(process.env.PORT || 3000);
+  console.log(
+    `🚀 Server running on http://localhost:${process.env.PORT || 3000}`,
+  );
 }
 bootstrap();
