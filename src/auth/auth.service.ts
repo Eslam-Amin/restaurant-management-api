4641b6cd8699b1ea42b4e7c269f1e24c5ebbafa9
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-19 00:54:03 +0300

    Created Auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,36 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { User, UserDocument } from '../users/user.schema';
+import * as bcrypt from 'bcryptjs';
+import { UsersService } from '../users/users.service';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(body: CreateUserDto): Promise<User> {
+    const { email, password } = body;
+    const existingUser = await this.usersService.findOneByEmail(email);
+    if (existingUser) throw new BadRequestException('Email in use');
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    body.password = hashedPassword;
+    const user = await this.usersService.createOne(body);
+    return user;
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.usersService.findOneByEmail(email);
+    if (!user) throw new NotFoundException('Invalid credentials');
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw new NotFoundException('Invalid credentials');
+
+    return user;
+  }
+}
