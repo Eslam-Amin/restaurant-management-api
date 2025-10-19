7ddf00d78fbac7a83099e6a9d5b986e54b1e31f1
Eslam Amin <eslam.amin.610@gmail.com>
2025-10-19 18:59:38 +0300

    Updated the default name of the database

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,6 @@
 export default () => ({
   mongo: {
     uri: process.env.MONGO_DB_URI || 'mongodb://localhost:27017',
-    dbName: process.env.MONGO_DB_NAME || 'restaurantdb',
+    dbName: process.env.MONGO_DB_NAME || 'restaurants_DB',
   },
 });
